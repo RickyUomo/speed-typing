5bae40ba52fe449e9f3883d3ad7175b3113a03ca
Ricky Kuo <rickykuo925@gmail.com>
2021-11-09 23:31:04 +0000

    first stage

--- src/App.js
+++ src/App.js
@@ -1,23 +1,68 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect, useRef } from 'react'
 
 function App() {
+
+  const [text, setText] = useState('')
+  const [wordCount, setWordCount] = useState('')
+  const [remainingTime, setRemainingTime] = useState(5)
+  const [isGameStart, setIsGameStart] = useState(false)
+  const textareaRef = useRef(null)
+
+  function handleChange(e) {
+    const { value } = e.target
+    setText(value)
+  }
+
+  function calculateWordCount(string) {
+    const res = string.split(' ')
+    setWordCount(res.filter(word => word !== '').length)
+  }
+
+  function startGame() {
+    setIsGameStart(true)
+    setRemainingTime(5)
+    setText('')
+    setWordCount('')
+    textareaRef.current.disabled = false
+    textareaRef.current.focus()
+  }
+
+  function endGame() {
+    calculateWordCount(text)
+    setIsGameStart(false)
+  }
+
+  useEffect(() => {
+    if (isGameStart && remainingTime > 0) {
+      setTimeout(() => {
+        setRemainingTime(oldtime => oldtime - 1)
+      }, 1000)
+    } else {
+      endGame()
+    }
+
+
+  }, [remainingTime, isGameStart])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Speed Typing Game</h1>
+      <textarea
+        ref={textareaRef}
+        type="text"
+        value={text}
+        onChange={handleChange}
+        disabled={!isGameStart}
+      />
+      <h4>Time Remaining: {remainingTime}</h4>
+      <button
+        onClick={startGame}
+        disabled={isGameStart}
+      >
+        Start
+      </button>
+      <h1>Words: {wordCount ? wordCount : '???'}</h1>
     </div>
   );
 }
